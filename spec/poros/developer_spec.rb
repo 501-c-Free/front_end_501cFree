27194cd3a1bd3d3e0e9f201efedc4809b22b3ed3
Conor Barthel <conor.barthel@gmail.com>
2022-04-15 10:57:10 -0600

    Feat: Developer poro

--- spec/poros/developer_spec.rb
+++ spec/poros/developer_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Developer do
-  data ={
-    id: '6',
-      attributes:{
-        name: 'Becky',
-        email: 'bn@gmail.com',
-        github: 'bn',
-        linkedin: 'b_n'
+  it "can create a developer object" do
+    data ={
+      id: '6',
+        attributes:{
+          name: 'Becky',
+          email: 'bn@gmail.com',
+          github: 'bn',
+          linkedin: 'b_n'
+        }
       }
-    }
-    dev = Developer.new(data)
+      dev = Developer.new(data)
 
-    expect(dev).to be_an_instance_of(Developer)
-    expect(dev.id).to eq("6")
-    expect(dev.name).to eq("Becky")
-    expect(dev.email).to eq('bn@gmail.com')
-    expect(dev.github).to eq('bn')
-    expect(dev.linkedin).to eq('b_n')
+      expect(dev).to be_an_instance_of(Developer)
+      expect(dev.id).to eq("6")
+      expect(dev.name).to eq("Becky")
+      expect(dev.email).to eq('bn@gmail.com')
+      expect(dev.github).to eq('bn')
+      expect(dev.linkedin).to eq('b_n')
+  end
 end
