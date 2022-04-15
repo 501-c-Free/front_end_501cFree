ddf7004dcc03d759965711387f704d83a9d8b9a0
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-15 16:34:54 -0400

    Feta: add commented code for future logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   def create
     @data = request.env["omniauth.auth"]
     SessionFacade.log_in(@data)
+    # @email = data[:info][:email]
+    # if SessionFacade.new_user(email)
+    #   redirect_to user_choice_path
+    # else
+    #   @data = request.env["omniauth.auth"]
+    #   SessionFacade.log_in(@data)
+    # end
   end
 
   def destroy
