ce56e71fc84baa2e859a72e8ae0ba2f8daf2e1bd
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-15 13:31:07 -0400

    Feat: add sesssions methods, facade and service

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
+  def new;end
 
   def create
+    @data = request.env["omniauth.auth"]
+    SessionFacade.log_in(@data)
+  end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:message] = 'You have been logged out'
+    redirect_to root_path
   end
 end
---
+++ app/services/session_service.rb
@@ -0,0 +1,16 @@
+class SessionService
+  def self.conn
+    Faraday.new(
+      url: 'https://localhost:3000/api/v1',
+      headers: {'Accept': 'application/json'})
+  end
+
+  def self.log_in(data)
+    # binding.pry
+    response = conn.post('/log_in') do |req|
+      req.params['email'] = data[:info][:email]
+      req.params['name'] = data[:info][:name]
+      req.params['access_token'] = data[:credentials][:token]
+    end
+  end
+end
