d3c91af9c20c18fae11477990e9069df86717136
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-14 21:59:19 -0400

    Feat: create routes for log_in/developers/user_choice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/non_profits/new"
+  post "/non_profits/create"
+
+  get "/developers/new"
+  post "/developers/create"
+
+  get "user_choice", to: "welcome#show"
+
+  get "/log_in", to: "sessions#new"
+  post "/log_in", to: "sessions#create"
+  delete "/log_out", to: "sessions#destroy"
 end
