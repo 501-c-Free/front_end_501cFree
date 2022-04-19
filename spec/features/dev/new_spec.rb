9bed5ea959e72435ca886666a99000a45bfaf42c
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-19 14:54:01 -0400

    Feat: add spec file for auth log in

--- spec/features/dev/new_spec.rb
+++ spec/features/dev/new_spec.rb
@@ -1,19 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'New developer page' do
-  xit "is linked to from the user choice page" do
-    visit user_choice_path
-    click_on 'Sign up'
-    expect(current_path).to eq(developers_new_path)
+  let!(:user) {user = User.new({attributes: {name: 'Tester',email: 'fake@user.com'}})}
+
+  before(:each) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.silence_get_warning = true
+    OmniAuth.config.add_mock(:google, {:uid => '12345'})
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    allow_any_instance_of(ApplicationController)
+        .to receive(:current_user).and_return(user)
   end
 
+  it "is linked to from the user choice page" do
+    stub_omniauth
+    visit root_path
+    click_on 'Sign Up'
+    expect(current_path).to eq(user_choice_path)
+  end
+
+
+  it 'logs in' do
+    stub_omniauth
+    visit root_path
+    click_on 'Log In'
+    expect(current_path).to eq(user_choice_path)
+  end
+
+  it 'bypasses to non_profit_path' do 
+    stub_omniauth
+    visit(non_profit_path)
+    save_and_open_page
+  end
+
+
   xit "has a form with name, email, github, and linkedIn fields" do
+
     visit developers_new_path
     expect(page).to have_content('Github')
     expect(page).to have_content('LinkedIn')
   end
 
-  it "when the form is filled out with valid data a new user is created on the
+  xit "when the form is filled out with valid data a new user is created on the
   back end and I am redirected to the user show page" do
     visit developers_new_path
     fill_in 'Github', with: 'cb'
