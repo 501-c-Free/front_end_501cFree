de127d59f5106b9781e32cab41c71850e54b0c47
Michael Brandt <msbrandt00@gmail.com>
2022-04-20 10:30:17 -0600

    post dev data to BE

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,11 +1,14 @@
 class DevelopersController < ApplicationController
   def new
     @user = current_user
-    DeveloperService.create_dev(@user)
+    DeveloperService.create_dev(@user, github = nil, linkedin = nil)
     # Now we have a fully created User object with associated Developer. Our view will have form for more information
   end
-
+  
   def create
+    @user = current_user
+    DeveloperService.create_dev(@user, params[:github], params[:linkedin])
+    # redirect_to developer_show - this route is not made yet? 
   end
 
   def show
