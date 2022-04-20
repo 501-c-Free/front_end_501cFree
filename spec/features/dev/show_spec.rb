fdfc2ae3985524360790f966cda409c33c2db85f
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-20 09:24:28 -0400

    Feat: add testing and fixutres

---
+++ spec/features/dev/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'New developer page' do
+  let!(:user) {@user = User.new({attributes: {user_id: 0, name: 'Tester',email: 'fake@user.com', type_of_user: 'developer'}})}
+
+  before(:each) do
+    @fake_user = DeveloperService.create_dev(@user)
+
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.silence_get_warning = true
+    OmniAuth.config.add_mock(:google, {:uid => '12345'})
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    allow_any_instance_of(ApplicationController)
+        .to receive(:current_user).and_return(user)
+
+
+  end
+
+  it "log in as developer" do
+    VCR.use_cassette('log_in_user') do
+      stub_omniauth
+      visit root_path
+      click_on 'Log In'
+      expect(current_path).to eq(developer_path)
+    end
+  end
+end
