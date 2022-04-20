c0a7b769f4e7aea6afdafe41a45e19fcad467aaf
Conor Barthel <conor.barthel@gmail.com>
2022-04-20 09:22:21 -0600

    Fix: resolve merge conflict

--- app/services/project_service.rb
+++ app/services/project_service.rb
@@ -1,4 +1,5 @@
 class ProjectService
+<<<<<<< HEAD
   def self.connect
     Faraday.new(url: ENV['env_url'])
   end
@@ -9,5 +10,19 @@ class ProjectService
       req.params['project_name'] = project_name
       req.params['description'] = description
     end
+=======
+  def conn
+    Faraday.new(url: ENV['env_url'])
+  end
+
+  def self.get_projects
+    response = conn.get('/api/v1/get_projects')
+    attrs = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_project(id)
+    response = conn.get('/api/v1/get_project')
+    attrs = JSON.parse(response.body, symbolize_names: true)
+>>>>>>> 3cde1fa (Feat: Project service #conn, #get_projects, #get_project)
   end
 end
