f2280f4934d527520762119475a3cd578c239298
Conor Barthel <conor.barthel@gmail.com>
2022-04-20 11:56:36 -0600

    Feat: Update Project facade

--- app/facades/project_facade.rb
+++ app/facades/project_facade.rb
@@ -1,7 +1,9 @@
 class ProjectFacade
   def self.get_all_projects
     json = ProjectService.get_all_projects
-    Project.new(json[:data])
+    json[:data][:attributes][:projects].map do |project_data|
+      Project.new(project_data)
+    end
   end
 
   def self.get_user_projects(id)
