bfc9d8044ed050fe76b6a793c1180061d85dd342
Michael Brandt <msbrandt00@gmail.com>
2022-04-21 11:13:51 -0600

    Refactor poro

--- app/poros/project.rb
+++ app/poros/project.rb
@@ -1,9 +1,11 @@
 class Project
-  attr_reader :id, :project_name, :description
+  attr_reader :project_id, :project_name, :description, :charity_id, :charity_name
 
   def initialize(data)
-    @id = data[:id]
-    @project_name = data[:project_name]
-    @description = data[:description]
+    @project_id = data[:id]
+    @project_name = data[:attributes][:project_name]
+    @description = data[:attributes][:description]
+    @charity_id = data[:attributes][:non_profit][:charity_id]
+    @charity_name = data[:attributes][:non_profit][:charity_name]
   end
 end
